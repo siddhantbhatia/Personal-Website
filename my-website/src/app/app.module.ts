f1621b6ab6f61eea8d0da77585427d2808d736e2
Siddhant <sid.rac125@gmail.com>
2019-10-04 01:51:40 +0800

    add responsiveness for 650px screen size

--- my-website/src/app/app.module.ts
+++ my-website/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IdeTypewriterComponent } from './ide-typewriter/ide-typewriter.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IdeTypewriterComponent
+    IdeTypewriterComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule

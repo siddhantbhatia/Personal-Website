e399dcecc3c87f2e8d14f66bc84e2ee2e9182597
Siddhant <sid.rac125@gmail.com>
2019-10-03 18:18:18 +0800

    remove redundant styles code

--- my-website/src/app/ide-typewriter/ide-typewriter.component.ts
+++ my-website/src/app/ide-typewriter/ide-typewriter.component.ts
@@ -13,16 +13,14 @@ export class IdeTypewriterComponent implements OnInit {
   ngOnInit() {
     const options = {
       strings: [
-        "<span style='color:deepskyblue'>var</span> <span style='color:white'>PartTimeHuman = {</span> \
-        <br>&emsp;&emsp;<span style='color:white'>name: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Sidhant Bhatia'</span><span>,</span>\
-        <br>&emsp;&emsp;<span style='color:white'>type: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Full Time CoderBoy'</span><span>,</span>\
-        <br>&emsp;&emsp;<span style='color:white'>email: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'sidhant3b@gmail.com'</span><span>,</span>\
-        <br>&emsp;&emsp;<span style='color:white'>phone: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'+60176434724'</span><span>,</span> \
-        <br>&emsp;&emsp;<span style='color:white'>HighAchieverScholarAt: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Monash University'</span><span>,</span>\
-        <br>&emsp;&emsp;<span style='color:white'>Summer2018InternAt: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Monster Jobs'</span><span>,</span>\
-        <br>&emsp;&emsp;<span style='color:white'>Summer2019InternAt: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Hire me now!'</span><span>,</span>\
-        <br>&emsp;&emsp;<span style='color:white'>location: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'KL, Malaysia'</span><span>,</span>\
-        <br><span style='color:white'>};</span>\
+        "<span style='color:deepskyblue'>var</span> <span>PartTimeHuman = {</span> \
+        <br>&emsp;&emsp;<span>type: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Full Time Coder'</span><span>,</span>\
+        <br>&emsp;&emsp;<span>email: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'sidhant3b@gmail.com'</span><span>,</span>\
+        <br>&emsp;&emsp;<span>phone: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'+60176434724'</span><span>,</span> \
+        <br>&emsp;&emsp;<span>HighAchieverScholarAt: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Monash University'</span><span>,</span>\
+        <br>&emsp;&emsp;<span>Summer2018InternAt: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Monster Jobs'</span><span>,</span>\
+        <br>&emsp;&emsp;<span>location: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'KL, Malaysia'</span><span>,</span>\
+        <br><span>};</span>\
         "
       ],
       typeSpeed: 1,

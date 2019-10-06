63005994b9ce90ee55d0fa36c0a2363030ad6b41
Siddhant <sid.rac125@gmail.com>
2019-10-06 22:50:39 +0800

    change profile photo and add resume link

--- my-website/src/app/ide-typewriter/ide-typewriter.component.ts
+++ my-website/src/app/ide-typewriter/ide-typewriter.component.ts
@@ -15,7 +15,7 @@ export class IdeTypewriterComponent implements OnInit {
       strings: [
         "<span style='color:deepskyblue'>var</span> <span>PartTimeHuman = {</span> \
         <br>&emsp;&emsp;<span>type: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Full Time Coder'</span><span>,</span>\
-        <br>&emsp;&emsp;<span>email: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'sidhant3b@gmail.com'</span><span>,</span>\
+        <br>&emsp;&emsp;<span>email: </span> <a style='color:rgba(253, 149, 90, 0.8); text-decoration: none' href='mailto:sidhant3b@gmail.com?Subject=Hey Sid!'>sidhant3b@gmail.com</a> <span>,</span>\
         <br>&emsp;&emsp;<span>phone: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'+60176434724'</span><span>,</span> \
         <br>&emsp;&emsp;<span>HighAchieverScholarAt: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Monash University'</span><span>,</span>\
         <br>&emsp;&emsp;<span>Summer2018InternAt: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Monster Jobs'</span><span>,</span>\

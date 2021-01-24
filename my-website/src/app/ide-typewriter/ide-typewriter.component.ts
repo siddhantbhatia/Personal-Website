dad47acf20317a7d210e86304f315a2d0fb450f0
siddhantbhatia <siddhant8b@gmail.com>
2021-01-24 14:39:07 +0800

    update work and name

--- my-website/src/app/ide-typewriter/ide-typewriter.component.ts
+++ my-website/src/app/ide-typewriter/ide-typewriter.component.ts
@@ -13,10 +13,10 @@ export class IdeTypewriterComponent implements OnInit {
   ngOnInit() {
     const options = {
       strings: [
-        "<span style='color:deepskyblue'>var</span> <span>PartTimeHuman = {</span> \
-        <br>&emsp;&emsp;<span>type: </span> <span style='color:rgba(253, 149, 90, 0.8)'>'Full Time Coder'</span><span>,</span>\
-        <br>&emsp;&emsp;<span>email: </span> <a style='color:rgba(253, 149, 90, 0.8); text-decoration: none' href='mailto:sidhant3b@gmail.com?Subject=Hey Sid!'>sidhant3b@gmail.com</a> <span>,</span>\
-        <br>&emsp;&emsp;<span>HighAchieverScholarAt: </span> <a href='https://www.monash.edu/it' target='_blank' style='color:rgba(253, 149, 90, 0.8); text-decoration: none'>'Monash University'</a><span>,</span>\
+        "<span style='color:deepskyblue'>var</span> <span>SoftwareEngineer = {</span> \
+        <br>&emsp;&emsp;<span>email: </span> <a style='color:rgba(253, 149, 90, 0.8); text-decoration: none' href='mailto:siddhant8b@gmail.com?Subject=Hey Sid!'>siddhant8b@gmail.com</a> <span>,</span>\
+        <br>&emsp;&emsp;<span>SolvingProblemsAt: </span> <a href='http://airasia.com/' target='_blank' style='color:rgba(253, 149, 90, 0.8); text-decoration: none'>'AirAsia Digital'</a><span>,</span>\
+        <br>&emsp;&emsp;<span>FirstClassHonoursFrom: </span> <a href='https://www.monash.edu/it' target='_blank' style='color:rgba(253, 149, 90, 0.8); text-decoration: none'>'Monash University'</a><span>,</span>\
         <br>&emsp;&emsp;<span>Winter2020InternAt: </span> <a href='https://warwick.ac.uk/fac/sci/statistics/research/' target='_blank' style='color:rgba(253, 149, 90, 0.8); text-decoration: none'>'Warwick Research Centre'</a><span>,</span>\
         <br>&emsp;&emsp;<span>Summer2019InternAt: </span> <a href='https://en.4th-valley.com/aboutus/' target='_blank' style='color:rgba(253, 149, 90, 0.8); text-decoration: none'>'Fourth Valley Concierge'</a><span>,</span>\
         <br>&emsp;&emsp;<span>Summer2018InternAt: </span> <a href='https://www.monster.com/about/' target='_blank' style='color:rgba(253, 149, 90, 0.8); text-decoration: none'>'Monster Jobs'</a><span>,</span>\
